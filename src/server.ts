6631730adad823d44dd20bf3d9b8c9e3b339e1f0
XuanChungLe <tieumoza09091993@gmail.com>
2022-10-11 04:50:50 +0700

    Update filteredimage endpoint

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,14 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import {filterImageFromURL, deleteLocalFiles} from './util/util';
+import express from "express";
+import bodyParser from "body-parser";
+import { filterImageFromURL, deleteLocalFiles } from "./util/util";
 
 (async () => {
-
   // Init the Express application
   const app = express();
 
   // Set the network port
   const port = process.env.PORT || 8082;
-  
+
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
@@ -28,19 +27,37 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
 
   /**************************************************************************** */
+  app.get(
+    "/filteredimage",
+    async (req: express.Request, res: express.Response) => {
+      let { image_url }: { image_url: string } = req.query;
 
+      if (!image_url) {
+        return res.status(400).send({ message: "Missing image url" });
+      }
+
+      filterImageFromURL(image_url)
+        .then((filtered_path) => {
+          res.sendFile(filtered_path, (err) => {
+            deleteLocalFiles([filtered_path]);
+          });
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    }
+  );
   //! END @TODO1
-  
+
   // Root Endpoint
   // Displays a simple message to the user
-  app.get( "/", async ( req, res ) => {
-    res.send("try GET /filteredimage?image_url={{}}")
-  } );
-  
+  app.get("/", async (req, res) => {
+    res.send("try GET /filteredimage?image_url={{}}");
+  });
 
   // Start the Server
-  app.listen( port, () => {
-      console.log( `server running http://localhost:${ port }` );
-      console.log( `press CTRL+C to stop server` );
-  } );
+  app.listen(port, () => {
+    console.log(`server running http://localhost:${port}`);
+    console.log(`press CTRL+C to stop server`);
+  });
 })();
